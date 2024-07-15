ef765c3486e0f950a7401a1cbd4e26947cc7ddcb
leo <leoknight.sg@gmail.com>
2024-07-15 21:45:20 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import Box from "./_components/Box";
+
+export default function Home() {
+  return (
+    <main>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </main>
+  );
+}
